091c212d9a5da814b82bc9f6cc706961de842e1c
samchat2561 saetieaw <samchat2561@gmail.com>
2025-09-16 10:18:41 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,7 @@ Route::get('/user', function (Request $request) {
 
 Route::post('register', [AuthController::class, 'register'])->name('register');
 Route::post('login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
